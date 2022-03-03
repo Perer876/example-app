183b73a76f8abba8e2bd4063d1eb3f39d0e42474
Oscar Arambula <oscar.arambula4388@alumnos.udg.mx>
2022-03-02 21:02:56 -0600

    El formulario formTask recuerda los datos introducidos al equivocarse

--- resources/views/tasks/formTask.blade.php
+++ resources/views/tasks/formTask.blade.php
@@ -11,20 +11,20 @@
     <form action="/tasks" method="post">
         @csrf
         <label for="user">Usuario</label>
-        <input type="text" name="user" id="user">
+        <input type="text" name="user" id="user" value="{{ old('user') }}">
         <br>
         <label for="priority">Prioridad</label>
         <select name="priority" id="priority">
-            <option value="1">Alta</option>
-            <option value="2">Media</option>
-            <option value="3">Baja</option>
+            <option value="1" {{ 1 == old('priority') ? 'selected' : ''}}>Alta</option>
+            <option value="2" {{ 2 == old('priority') ? 'selected' : ''}}>Media</option>
+            <option value="3" {{ 3 == old('priority') ? 'selected' : ''}}>Baja</option>
         </select>
         <br>
         <label for="description">Descripción</label>
-        <input type="text" name="description" id="description">
+        <input type="text" name="description" id="description" value="{{ old('description') }}">
         <br>
         <label for="deadline">Fecha</label>
-        <input type="text" name="deadline" id="deadline">
+        <input type="text" name="deadline" id="deadline" value="{{ old('deadline') }}">
         <br>
         <input type="submit" value="Guardar">
     </form>
